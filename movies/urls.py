d272a5d53e678b884e66afdc086c88ab403dd4f2
Alexander Morozov <reverse43@gmail.com>
2022-06-05 17:31:37 +0300

    Change links to plural versions

--- movies/urls.py
+++ movies/urls.py
@@ -4,19 +4,19 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = format_suffix_patterns([
-    path('movie/', views.MovieViewSet.as_view({'get': 'list'})),
-    path('movie/<int:pk>/', views.MovieViewSet.as_view({'get': 'retrieve'})),
-    path('review/', views.ReviewCreateViewSet.as_view({'post': 'create'})),
-    path('rating/', views.AddStarRatingViewSet.as_view({'post': 'create'})),
-    path('actor/', views.ActorsViewSet.as_view({'get': 'list'})),
-    path('actor/<int:pk>/', views.ActorsViewSet.as_view({'get': 'retrieve'})),
+    path('movies/', views.MovieViewSet.as_view({'get': 'list'})),
+    path('movies/<int:pk>/', views.MovieViewSet.as_view({'get': 'retrieve'})),
+    path('reviews/', views.ReviewCreateViewSet.as_view({'post': 'create'})),
+    path('ratings/', views.AddStarRatingViewSet.as_view({'post': 'create'})),
+    path('actors/', views.ActorsViewSet.as_view({'get': 'list'})),
+    path('actors/<int:pk>/', views.ActorsViewSet.as_view({'get': 'retrieve'})),
 ])
 
 # urlpatterns = [
-#     path('movie/', views.MovieListView.as_view()),
-#     path('movie/<int:pk>/', views.MovieDetailView.as_view()),
-#     path('review/', views.ReviewCreateView.as_view()),
-#     path('rating/', views.AddStarRatingView.as_view()),
+#     path('movies/', views.MovieListView.as_view()),
+#     path('movies/<int:pk>/', views.MovieDetailView.as_view()),
+#     path('reviews/', views.ReviewCreateView.as_view()),
+#     path('ratings/', views.AddStarRatingView.as_view()),
 #     path('actors/', views.ActorsDirectorsListView.as_view()),
 #     path('actors/<int:pk>/', views.ActorsDirectorsDetailView.as_view()),
 # ]
